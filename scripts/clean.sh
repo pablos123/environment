f12772f9d94c0feb709b57745ab30fa81f59be2b
Pablo <pablosaavedra123@gmail.com>
2023-04-18 16:34:18 -0300

    🍂

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
-sudo rm -f ../plays/keyring.deb
-sudo rm -f ../plays/lazygit
-sudo rm -f ../plays/lazygit.tar.gz
+sudo rm -f "$HOME/environment/plays/keyring.deb"
+sudo rm -f "$HOME/environment/plays/lazygit"
+sudo rm -f "$HOME/environment/plays/lazygit.tar.gz"
+
+sudo rm -f "$HOME/environment/scripts/lazygit"
+sudo rm -f "$HOME/environment/scripts/lazygit.tar.gz"
--- scripts/upgrade.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-sudo apt update
-sudo apt full-upgrade
-sudo flatpak update
-sudo apt autopurge
