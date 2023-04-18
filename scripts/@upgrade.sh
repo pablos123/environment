63001e4368858eeb4d91ebe3a067e03371f238a2
Pablo <pablosaavedra123@gmail.com>
2023-04-18 18:00:05 -0300

    🍂

--- scripts/@upgrade.sh
+++ scripts/@upgrade.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-source "$HOME/environment/scripts/fzf.sh"
-source "$HOME/environment/scripts/lazygit.sh"
+"$HOME/environment/scripts/fzf.sh"
+"$HOME/environment/scripts/lazygit.sh"
 
 sudo apt update
 sudo apt full-upgrade
 sudo flatpak update
 sudo apt autopurge
 
-source "$HOME/environment/scripts/clean.sh"
+"$HOME/environment/scripts/clean.sh"
