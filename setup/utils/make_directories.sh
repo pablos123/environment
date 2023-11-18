4164f7a533ab8f1a44082ec1ee0a666957ca83d7
Pablo <pablosaavedra123@gmail.com>
2023-11-18 19:46:49 -0300

    🍂

--- setup/utils/make_directories.sh
+++ setup/utils/make_directories.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-mkdir -p "$HOME/images/"
-mkdir -p "$HOME/.audio/"
-mkdir -p "$HOME/.video_wallpapers/"
-mkdir -p "$HOME/projects/"
+mkdir -p "$HOME/images/" "$HOME/.audio/" "$HOME/.video_wallpapers/" "$HOME/projects/"
