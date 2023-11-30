412f055edbff0711c7bc40b080cad7516578db5a
Pablo <pablosaavedra123@gmail.com>
2023-11-30 19:42:23 -0300

    🍂

--- setup/installers/repos.sh
+++ setup/installers/repos.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#
-#
+
 declare -A repos=(
     ["git@github.com:pablos123/notes.git"]="$HOME/notes"
-    ["git@github.com:pablos123/.media.git"]="$HOME/.media"
     ["git@github.com:pablos123/cinecli.git"]="$HOME/projects/cinecli"
     ["git@github.com:pablos123/dump.git"]="$HOME/projects/dump"
     ["git@github.com:pablos123/pablos123.github.io.nvim.git"]="$HOME/projects/pablos123.github.io"
@@ -17,3 +15,6 @@ for repo in "${!repos[@]}"; do
     fi
     git clone "$repo" "${repos[$repo]}"
 done
+
+rm -rf "$HOME/.media"
+git clone "git@github.com:pablos123/.media.git" "$HOME/.media"
