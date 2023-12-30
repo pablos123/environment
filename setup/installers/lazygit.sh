bede7f7ea53220cfa49f3a1f88cb7a946509ebb9
Pablo <pablosaavedra123@gmail.com>
2023-12-30 18:28:18 -0300

    fix lazygit installer

--- setup/installers/lazygit.sh
+++ setup/installers/lazygit.sh
@@ -2,8 +2,8 @@
 #
 # https://github.com/jesseduffield/lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo ./lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf ./lazygit.tar.gz ./lazygit
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 rm -f ./lazygit
 rm -f ./lazygit.tar.gz
