f42a2dd5d166c4829de8f3f40f9c32e8434b3535
Pablo <pablosaavedra123@gmail.com>
2024-02-06 09:26:53 -0300

    🍂

--- setup/installers/wezterm.sh
+++ setup/installers/wezterm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
-wget -O './wezterm.deb' 'https://github.com/wez/wezterm/releases/download/20240127-113634-bbcac864/wezterm-20240127-113634-bbcac864.Ubuntu22.04.deb'
+wget -O './wezterm.deb' 'https://github.com/wez/wezterm/releases/download/20240203-110809-5046fc22/wezterm-20240203-110809-5046fc22.Ubuntu22.04.deb'
 sudo apt install -y './wezterm.deb'
 rm -f './wezterm.deb'
