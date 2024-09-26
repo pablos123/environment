675c534a4da4972ec3fac31aba2c7dda864600b1
psaavedra <psaavedra@educativa.com>
2024-09-26 16:45:04 -0300

    genc

--- dotfiles/config/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/config/.config/nvim/lua/plugins/lsp.lua
@@ -1,17 +1,21 @@
 return {
     {
-        {
-            "williamboman/mason.nvim",
-            config = function()
-                require("mason").setup {}
-            end
-        },
-        {
-            "williamboman/mason-lspconfig.nvim",
-            config = function()
-                require("mason-lspconfig").setup {}
-            end
-        },
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup {}
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup {}
+        end
+    },
+    {
         "neovim/nvim-lspconfig",
-    }
+        config = function()
+            require("lspconfig").bashls.setup {}
+            require("lspconfig").ansiblels.setup {}
+        end
+    },
 }
