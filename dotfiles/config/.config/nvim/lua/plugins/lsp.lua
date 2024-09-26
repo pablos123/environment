7e762d1956929aee1ab43e1160c1c3198e8716ae
Pablo <pablosaavedra123@gmail.com>
2024-09-26 17:34:58 -0300

    genc

--- dotfiles/config/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/config/.config/nvim/lua/plugins/lsp.lua
@@ -16,6 +16,7 @@ return {
         config = function()
             require("lspconfig").bashls.setup {}
             require("lspconfig").ansiblels.setup {}
+            require("lspconfig").ts_ls.setup {}
         end
     },
 }
