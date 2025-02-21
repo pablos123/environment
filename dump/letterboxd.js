9f0f05076dfa585e2795418113fab3a200af4b77
Pablo <pablosaavedra123@gmail.com>
2025-02-21 19:40:26 -0300

    genc

--- dump/letterboxd.js
+++
@@ -1,2 +0,0 @@
-// Delete all ratings of the diary page and go the next one.
-document.querySelectorAll(".remove-rating.tooltip").forEach( e => e.click()); document.querySelectorAll(".next").forEach( e => e.click())
