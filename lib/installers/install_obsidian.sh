5848adc7cfd0b38b505653222a6c81850b39611a
Pablo <pablosaavedra123@gmail.com>
2025-02-21 19:24:23 -0300

    genc

--- lib/installers/install_obsidian.sh
+++ lib/installers/install_obsidian.sh
@@ -5,19 +5,24 @@ function obtain_obsidian_version() {
 }
 
 function install_obsidian() {
-    local current_version latest_version obsidian_path
+    local current_version latest_version current_version_path latest_version_path obsidian_path
 
+    obsidian_path="${HOME}/bin"
+
+    current_version=$(obtain_obsidian_version "$(find "${obsidian_path}" -name "*obsidian_v*")")
     latest_version=$(obtain_obsidian_version "$(curl https://github.com/obsidianmd/obsidian-releases/releases/ 2>/dev/null)")
-    current_version=$(obtain_obsidian_version "$(find "${HOME}/bin/" -name "*obsidianv*")")
+
+    current_version_path="${obsidian_path}/obsidian_v${current_version}"
+    latest_version_path="${obsidian_path}/obsidian_v${latest_version}"
 
     if [[ -z "${current_version}" ]] || [[ "${latest_version}" > "${current_version}" ]]; then
-        rm -f "${HOME}/bin/obsidianv${current_version}"
-        wget -O "${HOME}/bin/obsidianv${latest_version}" "https://github.com/obsidianmd/obsidian-releases/releases/download/v${latest_version}/Obsidian-${latest_version}.AppImage"
+        rm -f "${current_version_path}"
+        wget -O "${latest_version_path}" "https://github.com/obsidianmd/obsidian-releases/releases/download/v${latest_version}/Obsidian-${latest_version}.AppImage"
     else
         echo "Obsidian is in the latest version: ${latest_version}"
     fi
 
-    chmod +x "${HOME}/bin/obsidianv${latest_version}"
+    chmod +x "${latest_version_path}"
 }
 
 install_obsidian
