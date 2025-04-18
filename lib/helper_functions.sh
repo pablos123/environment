b57c091d74de9743b2308e5be3b1f81ae373948d
Pablo <pablosaavedra123@gmail.com>
2025-04-18 13:01:55 -0300

    genc

--- lib/helper_functions.sh
+++ lib/helper_functions.sh
@@ -9,12 +9,12 @@ function calculate_applet_position() {
     x_size="${1}"
     y_size="${2}"
 
-    x_gap=16
-    y_gap=42
-
     [[ -z "${x_size}" ]] || [[ ! "${x_size}" =~ ^[0-9]+$ ]] ||
     [[ -z "${y_size}" ]] || [[ ! "${y_size}" =~ ^[0-9]+$ ]] &&
         exit 1
+
+    x_gap=16
+    y_gap=42
 
     # array: x y offset_x offset_y
     mapfile -t res < <(xrandr | grep 'connected primary' | awk '{print $4}' | tr x+ '\n\n')
