ae526252f402f54e8d1a71aae8e357a94bdd8564
psaavedra <psaavedra@educativa.com>
2025-06-27 15:22:25 -0300

    genc

--- lib/installers/install_wezterm.sh
+++ lib/installers/install_wezterm.sh
@@ -2,6 +2,7 @@
 
 function install_wezterm() {
     sudo mkdir -p /etc/apt/keyrings
+    sudo chmod 0755 /etc/apt/keyrings
     curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
     (echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list) > /dev/null
     sudo apt-get update
