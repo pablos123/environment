87490dbf59f620eb664a467bc701e77618167fd5
psaavedra <psaavedra@educativa.com>
2025-09-10 11:59:46 -0300

    genc

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -8,25 +8,34 @@ alias sl='ls'
 alias ll='ls -l'
 alias la='ls -a'
 alias lla='ls -al'
-alias mkdir='mkdir -p -v'
-alias rm='rm -i'
-alias tree='tree --dirsfirst --gitignore -F -C -A'
-alias xfe='(xfe . &>> /dev/null) & disown'
+
 alias ..='cd -- ..'
 alias ...='cd -- ../..'
 alias ....='cd -- ../../..'
+
 alias :q='exit'
 alias :wq='exit'
+
 alias vim='nvim'
+alias im='vim'
+alias mvi='vim'
+alias miv='vim'
+
 alias bat='batcat --theme=gruvbox-dark --paging=never --decorations=never'
 alias cat='bat'
+
 alias duf='duf --style=ascii --theme=ansi'
 alias df='duf'
-# Git
+
+alias mkdir='mkdir -p -v'
+alias rm='rm -i'
+
 alias genc='git add . && git commit -m "genc"'
-# Others
+
 alias bigdirs='(sudo du -h / | sort -rh | head -n 15) 2> /dev/null'
 alias myip='printf "External: " && curl -s ifconfig.me && echo && printf "Local: " && hostname -I'
 alias sources='grep --color=always -v -E "^#|^ *$" /etc/apt/sources.list /etc/apt/sources.list.d/*'
 alias weather='curl wttr.in/rosario'
 alias calendar_fact='calendar | head -n 1 | cowsay -f duck | lolcat'
+alias tree='tree --dirsfirst --gitignore -F -C -A'
+alias xfe='(xfe . &>> /dev/null) & disown'
