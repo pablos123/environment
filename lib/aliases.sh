e3c5ea93fd02bc8ff7db1ed750f7f84c278eadc8
Pablo <pablosaavedra123@gmail.com>
2025-09-15 02:44:38 -0300

    genc

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 #
 # Common
+<<<<<<< Updated upstream
 alias ls='eza --sort=extension --extended --group-directories-first --classify --git'
 alias l='ls'
 alias s='ls'
@@ -39,3 +40,39 @@ alias weather='curl wttr.in/rosario'
 alias calendar_fact='calendar | head -n 1 | cowsay -f duck | lolcat'
 alias tree='tree --dirsfirst --gitignore -F -C -A'
 alias xfe='(xfe . &>> /dev/null) & disown'
+=======
+alias ls="eza --icons --extended --group-directories-first -F"
+alias l="ls"
+alias s="ls"
+alias sl="ls"
+alias ll="ls -l"
+alias la="ls -a"
+alias lla="ls -al"
+alias mkdir="mkdir -p -v"
+alias rm="rm -i"
+alias tree="tree --dirsfirst --gitignore -F -C -A"
+alias xfe="(xfe . &>> /dev/null) & disown"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias :q="exit"
+alias :wq="exit"
+alias vim="nvim"
+alias bat="batcat --theme=gruvbox-dark --paging=never --color=always --decorations=never"
+alias cat="bat"
+alias df="duf"
+# Git
+alias genc="git add . && git commit -m 'genc'"
+alias ga="git add"
+alias gs="git status"
+alias gl="git log"
+alias gc="git commit"
+# Others
+alias bigdirs="(sudo du -h / | sort -rh | head -n 15) 2> /dev/null"
+alias myip="printf 'External: ' && curl -s ifconfig.me && echo && printf 'Local: ' && hostname -I"
+alias sources="grep --color=always -v -E '^#|^ *$' /etc/apt/sources.list /etc/apt/sources.list.d/*"
+alias weather="curl wttr.in/rosario"
+alias calendar_fact="calendar | head -n 1 | cowsay -f duck | lolcat"
+alias tmux="tmux -2" # Tell tmux to assume 256 colors
+alias download_torrent="aria2c --dir /media/pab/wd_4tb_hdd_int/movies/"
+>>>>>>> Stashed changes
