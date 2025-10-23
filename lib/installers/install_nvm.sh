0d42cabdd8f73fc3116d13fc4b5e32487458b26d
psaavedra <psaavedra@educativa.com>
2025-10-23 11:01:23 -0300

    genc

--- lib/installers/install_nvm.sh
+++ lib/installers/install_nvm.sh
@@ -3,20 +3,21 @@
 function install_nvm() {
     local node_version
 
+    export NVM_DIR=/opt/nvm
+
     mkdir -p "${NVM_DIR}"
 
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
-    [[ ! -s "${NVM_DIR}/nvm.sh" ]] || exit 1
-
     source "${NVM_DIR}/nvm.sh"
+
     nvm install --lts
 
     sleep 0.5
 
     node_version="$(node --version)"
     ln -sf "${NVM_DIR}/versions/node/${node_version}/bin/node" /usr/bin/node
-    ln -sf "${NVM_DIR}/versions/node/v${node_version}/bin/npm" /usr/bin/npm
+    ln -sf "${NVM_DIR}/versions/node/${node_version}/bin/npm" /usr/bin/npm
 }
 
-sudo bash -c -- "export NVM_DIR=/opt/nvm; $(declare -f install_nvm); install_nvm"
+sudo bash -c -- "$(declare -f install_nvm); install_nvm"
