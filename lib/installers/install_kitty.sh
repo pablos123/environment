67da8e319d76cd155baaf143df062d298e99011b
psaavedra <psaavedra@educativa.com>
2025-12-30 14:43:51 -0300

    genc

--- lib/installers/install_kitty.sh
+++ lib/installers/install_kitty.sh
@@ -2,8 +2,8 @@
 
 function install_kitty() {
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
-    ln -s "${HOME}/.local/kitty.app/bin/kitty" "${HOME}/bin/kitty"
-    ln -s "${HOME}/.local/kitty.app/bin/kitten" "${HOME}/bin/kitten"
+    ln -sf "${HOME}/.local/kitty.app/bin/kitty" "${HOME}/bin/kitty"
+    ln -sf "${HOME}/.local/kitty.app/bin/kitten" "${HOME}/bin/kitten"
 }
 
 install_kitty
