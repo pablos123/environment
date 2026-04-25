f1c5339b7722878799127e12f0660517a908b762
Pablo <pablosaavedra123@gmail.com>
2026-04-25 19:39:49 -0300

    genc

--- lib/installers/obsidian.bash
+++ lib/installers/obsidian.bash
@@ -30,7 +30,7 @@ if [[ -n "${LATEST_VERSION}" && "${LATEST_VERSION}" != "${CURRENT_VERSION}" ]]; 
     CURRENT_VERSION_PATH="${OBSIDIAN_PATH}/obsidian_v${CURRENT_VERSION}"
     LATEST_VERSION_PATH="${OBSIDIAN_PATH}/obsidian_v${LATEST_VERSION}"
 
-    curl --no-progress-meter --output "${LATEST_VERSION_PATH}" "https://github.com/obsidianmd/obsidian-releases/releases/download/v${LATEST_VERSION}/Obsidian-${LATEST_VERSION}.AppImage" || true
+    curl --no-progress-meter --location --output "${LATEST_VERSION_PATH}" "https://github.com/obsidianmd/obsidian-releases/releases/download/v${LATEST_VERSION}/Obsidian-${LATEST_VERSION}.AppImage" || true
 
     if [[ -s "${LATEST_VERSION_PATH}" ]]; then
         chmod +x "${LATEST_VERSION_PATH}" || true
